0460ca2fde4e0fb4213673e9d7658ac407a46856
baiyaguang <301070179@qq.com>
2019-08-15 10:19:08 +0800

    fastjson 处理Date格式

--- springboot-redis-demo/src/test/java/com/beng/test/Test.java
+++ springboot-redis-demo/src/test/java/com/beng/test/Test.java
@@ -1,9 +1,17 @@
 package com.beng.test;
 
+import java.util.Date;
+
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.SimpleDateFormatSerializer;
 
 import net.sourceforge.groboutils.junit.v1.MultiThreadedTestRunner;
 import net.sourceforge.groboutils.junit.v1.TestRunnable;
@@ -41,5 +49,35 @@ public class Test {
         } catch (Throwable e) {
             e.printStackTrace();
         }
+    }
+
+    private static SerializeConfig mapping = new SerializeConfig();
+    private static String dateFormat;
+    static {
+        dateFormat = "yyyy-MM-dd HH:mm:ss";
+    }
+
+    @org.junit.Test
+    public void testJSon() {
+        JSONObject json1 = new JSONObject();
+        json1.put("date", new Date());
+        System.out.println(json1.toJSONString());
+        System.out.println(JSONObject.toJSONString(json1));
+        System.out.println(toJSON(json1));
+    }
+
+    public static String toJSON(Object jsonText) {
+        return JSON.toJSONString(jsonText, SerializerFeature.WriteDateUseDateFormat);
+    }
+
+    /**
+     * 自定义时间格式
+     * 
+     * @param jsonText
+     * @return
+     */
+    public static String toJSON(String dateFormat, String jsonText) {
+        mapping.put(Date.class, new SimpleDateFormatSerializer(dateFormat));
+        return JSON.toJSONString(jsonText, mapping);
     }
 }
